885294fc7bbe2ecc3ce1cc0042362466d4de2664
Heath <email address>
2018-12-11 08:13:26 -0600

    added more dots and the ability for the dots to move upward

--- pong_heath.py
+++ pong_heath.py
@@ -4,7 +4,24 @@ sense = SenseHat()
 white = (255, 255, 255)
 
 bat_y = 4
-sense.set_pixel(0, bat_y, white)
+
+def draw_bat():
+    sense.set_pixel(0, bat_y, white)
+    sense.set_pixel(0, bat_y + 1, white)
+    sense.set_pixel(0, bat_y - 1, white)
+
+draw_bat()
+
+def move_up(event):
+    global bat_y
+    if event.action == 'pressed':
+        bat_y -= 1
+
+while True:
+    draw_bat()
+    sense.stick.direction_up = move_up
+
+
 
 
 
