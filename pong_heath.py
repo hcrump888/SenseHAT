52dd57626ed100af0d62e523bc160fdeda9355a8
Connor <crichardson914@lsr7.net>
2018-12-13 08:20:16 -0600

    deleted clones and added ball moving.

--- pong_heath.py
+++ pong_heath.py
@@ -24,15 +24,32 @@ def move_up(event):
     if event.action == 'pressed' and bat_y > 1:
         bat_y -= 1
 
+def move_down(event):
+    global bat_y
+    if event.action == 'pressed' and bat_y > 1:
+        bat_y -= -1
+
 sense.clear(0, 0, 0)
 
 def draw_ball():
     sense.set_pixel(ball_position[0], ball_position[1], blue)
+    ball_position[0] += ball_velocity[0]
+    if ball_position[0] == 7 or  ball_position[0] == 0:
+        ball_velocity[0] = -ball_velocity[0]
+    if ball_position[0] == 7 or ball_position[0] == 0:
+        ball_position[0] = -ball_velocity[0]
+    ball_position[1] += ball_velocity[1]
+    if ball_position[1] == 7 or ball_position[1] == 0:
+        ball_velocity[1] = -ball_velocity[1]
+
+
 
 while True:
     draw_bat()
     sense.stick.direction_up = move_up
+    sense.stick.direction_down = move_down
     sleep(0.25)
+    sense.clear(0, 0, 0)
     draw_ball()
 
 
